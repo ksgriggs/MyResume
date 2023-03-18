8ecb984bb04890c9c9035c238cf309c579187bfb
Scott Griggs <sgriggs@gmail.com>
2023-03-17 21:37:21 -0400

    Updated counter link

--- dist/js/visitors.js
+++ dist/js/visitors.js
@@ -1,8 +1,12 @@
 let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-        document.getElementById("visits").innerHTML = xhttp.responseText;
-    }
+  if (this.readyState === 4 && this.status === 200) {
+    document.getElementById("visits").innerHTML = xhttp.responseText;
+  }
 };
-xhttp.open("Get", "https://ytum3c1qse.execute-api.us-east-1.amazonaws.com/Prod/counter/", true)
+xhttp.open(
+  "Get",
+  "https://tvcvbl9use.execute-api.us-east-1.amazonaws.com/Prod/counter/",
+  true
+);
 xhttp.send();
